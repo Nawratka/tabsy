f4306309d0cb5c7067c5419c6a0364886a03206e
Kamila <kamilanawrat@gmail.com>
2024-02-29 10:04:57 +0100

    styles improved

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const tabs = document.querySelector('.tabs');
+const tabContainer = document.querySelector('.tabs');
 
-tabs.addEventListener('click', (e) => {
+tabContainer.addEventListener('click', (e) => {
 	if (!e.target.classList.contains('tab')) return;
 
-	const contents = document.querySelectorAll('.content');
+	// TAB HANDLE
+	document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
 	const tab = e.target.dataset.tab;
+	const currTab = document.querySelector(`p[data-tab=${tab}]`);
+	currTab.classList.add('active');
+
+	// CONTENT HANDLE
+	const contents = document.querySelectorAll('.content');
 	contents.forEach((div) => div.classList.add('hidden'));
 	const currContent = document.querySelector(`div[data-tab=${tab}]`);
 	currContent.classList.remove('hidden');
